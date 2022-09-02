e275367482c65ab8ed136af209200d823d514c4d
Ced <toullier.gauthier@protonmail.com>
2022-09-02 09:55:16 -0400

    New article elements

--- lib/article.dart
+++ lib/article.dart
@@ -4,7 +4,19 @@ class Article {
 
   String name;
   String price;
-  Map<String, bool?> colocataire = {};
+  Map<String, bool> colocataire = {};
+
+  int getColoc() {
+    List<bool> nbrColoc = colocataire.values.toList();
+
+    int index = 0;
+    for(bool statut in nbrColoc) {
+      if (statut == true){
+        index++;
+      }
+    }
+    return index;
+  }
 
   Article(this.name, this.price, this.colocataire);
 }
