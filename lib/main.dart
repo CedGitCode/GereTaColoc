e514457d10624e2c3f8688993457b9fa1977094c
natha <natha@NATHAN_PC_PORT>
2022-09-19 13:30:52 -0400

    Finished Backend

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        theme: ThemeData(
+          brightness: Brightness.light,
+          /* light theme settings */
+        ),
+        darkTheme: ThemeData(
+          brightness: Brightness.dark,
+          /* dark theme settings */
+        ),
+        themeMode: ThemeMode.system,
+        /* ThemeMode.system to follow system theme,
+         ThemeMode.light for light theme,
+         ThemeMode.dark for dark theme
+      */
         title:"Gère ta coloc",
         home:MyHomePage()
     );
